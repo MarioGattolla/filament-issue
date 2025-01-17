49639dcac495e2f00a392819e8731d89463c689d
mario <mario.gattolla@gmail.com>
2025-01-17 12:24:45 +0100

    fix

--- app/Filament/Resources/InvoiceResource.php
+++ app/Filament/Resources/InvoiceResource.php
@@ -38,7 +38,16 @@ class InvoiceResource extends Resource
                         Placeholder::make('updated_at')
                             ->label('Last Modified Date')
                             ->content(fn(?Invoice $record): string => $record?->updated_at?->diffForHumans() ?? '-'),
-                    ])
+                    ]),
+                        Tab::make('second tab')->schema([
+                            Placeholder::make('created_at')
+                                ->label('Created Date')
+                                ->content(fn(?Invoice $record): string => $record?->created_at?->diffForHumans() ?? '-'),
+
+                            Placeholder::make('updated_at')
+                                ->label('Last Modified Date')
+                                ->content(fn(?Invoice $record): string => $record?->updated_at?->diffForHumans() ?? '-'),
+                        ])
                 ]),
             ]);
     }
